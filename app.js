52ecee380c2d3c6592d6a2a8a87c4ec568a55afe
Toni Halmetoja <36439969+ToniHalmetoja@users.noreply.github.com>
2022-03-30 11:32:37 +0200

    recommendations starting

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var dotenv = require("dotenv").config()
 const MongoClient = require("mongodb").MongoClient;
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var recommendationsRouter = require('./routes/recommendations');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/recommendations', recommendationsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
